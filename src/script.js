a1b26c8b391b2fc856a4f9aa3c7e84211f57b3bd
mudakakk <161918354+mudakakk@users.noreply.github.com>
2025-06-28 09:22:25 +0000

    did a lot of styling and added dark mode

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-import "./styles.css"
+import "./styles.css";
+
+const darkModeBtn = document.querySelector(".dark-light");
+const html = document.querySelector("html");
+
+darkModeBtn.addEventListener("click", (e) => {
+    if (html.classList.contains("dark")) {
+        darkModeBtn.textContent = "Dark mode";
+    } else {
+        darkModeBtn.textContent = "Light mode";
+    }
+    html.classList.toggle("dark");
+});
